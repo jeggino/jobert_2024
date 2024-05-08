3c6f3239b5cf64c14dd4f923e5800ba5fc0a8ba1
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-08 09:56:41 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -43,12 +43,12 @@ db_survey = deta.Base("db_survey")
 drive = deta.Drive("df_pictures")
 
 # --- FUNCTIONS ---
-db_content_observations = pd.DataFrame(db_observations.fetch().items)
-db_content_surveys = pd.DataFrame(db_survey.fetch().items)
+# db_content_observations = pd.DataFrame(db_observations.fetch().items)
+# db_content_surveys = pd.DataFrame(db_survey.fetch().items)
 
-project = st.selectbox("Project", ["Zaandam","Badhoevedorp"],key="project")
-db_content_observations = db_content_observations[db_content_observations["project"]==project]
+# project = st.selectbox("Project", ["Zaandam","Badhoevedorp"],key="project")
+# db_content_observations = db_content_observations[db_content_observations["project"]==project]
 
 
-st.dataframe(data=db_content_observations, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
+# st.dataframe(data=db_content_observations, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
 st.dataframe(data=db_content_surveys, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
