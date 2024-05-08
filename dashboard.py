aaf3e3c64283362d7b290f53c41811629e02df2b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-08 15:17:13 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -54,3 +54,35 @@ db_content_surveys = db_content_surveys[db_content_surveys["Locatie"]==project]
 
 st.dataframe(data=db_content_observations, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
 st.dataframe(data=db_content_surveys, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
+
+
+ICON_URL = "https://upload.wikimedia.org/wikipedia/commons/c/c4/Projet_bi%C3%A8re_logo_v2.png"
+
+icon_data = {
+    # Icon from Wikimedia, used the Creative Commons Attribution-Share Alike 3.0
+    # Unported, 2.5 Generic, 2.0 Generic and 1.0 Generic licenses
+    "url": ICON_URL,
+    "width": 242,
+    "height": 242,
+    "anchorY": 242,
+}
+
+data =db_content_observations
+data["icon_data"] = None
+for i in data.index:
+    data["icon_data"][i] = icon_data
+
+view_state = pdk.data_utils.compute_view(data[["lng", "lat"]], 0.1)
+
+icon_layer = pdk.Layer(
+    type="IconLayer",
+    data=data,
+    get_icon="icon_data",
+    get_size=4,
+    size_scale=15,
+    get_position=["lon", "lat"],
+    pickable=True,
+)
+
+r = pdk.Deck(layers=[icon_layer], initial_view_state=view_state, tooltip={"text": "{sp}"})
+st.pydeck_chart(r,use_container_width=True)
