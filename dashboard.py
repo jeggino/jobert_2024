288ce651c4850056e3039ec13fecc3bdf0a40441
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-08 09:58:43 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -39,16 +39,16 @@ st.markdown(
 # --- CONNECT TO DETA ---
 deta = Deta(st.secrets["deta_key"])
 db_observations = deta.Base("df_observations")
-db_survey = deta.Base("db_survey")
+db_survey = deta.Base("df_survey")
 drive = deta.Drive("df_pictures")
 
 # --- FUNCTIONS ---
-# db_content_observations = pd.DataFrame(db_observations.fetch().items)
+db_content_observations = pd.DataFrame(db_observations.fetch().items)
 db_content_surveys = pd.DataFrame(db_survey.fetch().items)
 
-# project = st.selectbox("Project", ["Zaandam","Badhoevedorp"],key="project")
-# db_content_observations = db_content_observations[db_content_observations["project"]==project]
+project = st.selectbox("Project", ["Zaandam","Badhoevedorp"],key="project")
+db_content_observations = db_content_observations[db_content_observations["project"]==project]
 
 
-# st.dataframe(data=db_content_observations, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
+st.dataframe(data=db_content_observations, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
 st.dataframe(data=db_content_surveys, width=None, height=None, use_container_width=False, hide_index=True, column_order=None, column_config=None)
