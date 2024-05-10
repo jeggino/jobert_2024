e22da18e419b18f55b3d1554abfa423a0e8da787
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-11 00:09:16 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -124,3 +124,12 @@ def vote(item):
     if st.button("Submit"):
         st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()
+
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
