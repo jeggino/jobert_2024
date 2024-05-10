b11ccc1fce59ed89dbae9779c2353885be1faacf
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-11 00:07:54 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -114,4 +114,13 @@ with tab3:
             st.image(res)
     except:
         st.warning("no files")
-        
+
+
+
+@st.experimental_dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
