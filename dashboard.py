d5e0a6c88ae0a6920c24f03238c9087e9a586fe5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-10 12:50:02 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -106,7 +106,7 @@ with tab3:
             if title:
                 
                 bytes_data = uploaded_file.getvalue()
-                drive.put(f"{title}.jpeg", data=bytes_data)
+                drive.put(f"{title}.jpg", data=bytes_data)
     except:
         st.warning("upload a file")
 
