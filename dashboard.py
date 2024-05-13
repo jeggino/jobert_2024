9e7169e5444b0b958e5fed0ee2eae59677a6aab5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-13 11:42:30 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -49,7 +49,7 @@ ICON = {"Gierzwaluw":"https://cdn-icons-png.flaticon.com/128/732/732126.png",
         "Swift_nest": "icons/swift_nest.jpg"}
 
 OUTPUT_height = 800
-OUTPUT_width = 950
+OUTPUT_width = 1150
 ICON_SIZE = (18,18)
 ZOOM = 20
 
