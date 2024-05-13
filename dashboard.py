33fada62df9ede691dd2bfba9c300c597ecbabf5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-13 11:30:09 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 import folium
-from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
+from folium.plugins import Fullscreen, LocateControl
 from streamlit_folium import st_folium
 
 import pandas as pd
@@ -49,6 +49,72 @@ ICON = {"Gierzwaluw":"https://cdn-icons-png.flaticon.com/128/732/732126.png",
         "Swift_nest": "icons/swift_nest.jpg"}
 
 ICON_SIZE = (18,18)
+
+# --- FUNCTIONS ---
+def popup_html(row):
+    
+    i = row
+     
+    datum=df_2['datum'].iloc[i] 
+    soortgroup=df_2['soortgroup'].iloc[i]
+    sp = df_2['sp'].iloc[i] 
+    functie=df_2['functie'].iloc[i]
+    gedrag=df_2['gedrag'].iloc[i]
+    verblijf=df_2['verblijf'].iloc[i]
+    bewoond=df_2['onbewoond'].iloc[i] 
+    opmerking=df_2['opmerking'].iloc[i]
+    aantal=df_2['aantal'].iloc[i]
+    waarnemer=df_2['waarnemer'].iloc[i] 
+       
+
+    left_col_color = "#19a7bd"
+    right_col_color = "#f2f0d3"
+    
+    html = """<!DOCTYPE html>
+    <html>
+    <table style="height: 126px; width: 300;">
+    <tbody>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soortgroup</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(soortgroup) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soort</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(sp) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Gedrag</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(gedrag) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Bewoond</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(bewoond) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Aantal</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(aantal) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
+    </tr>
+    </tbody>
+    </table>
+    </html>
+    """
+    return html
 
 
 # --- CONNECT TO DETA ---
@@ -129,14 +195,11 @@ elif selected == '🗺️ Kaart':
         
         
         map = folium.Map()
-        LocateControl(auto_start=True).add_to(map)
         Fullscreen().add_to(map)
         
-        fg = folium.FeatureGroup(name="Vleermuiskast")
         fg_2 = folium.FeatureGroup(name="Huismussen")
         fg_4 = folium.FeatureGroup(name="Gierzwaluwen")
         fg_3 = folium.FeatureGroup(name="Vleermuizen")
-        map.add_child(fg)
         map.add_child(fg_2)
         map.add_child(fg_4)
         map.add_child(fg_3)
