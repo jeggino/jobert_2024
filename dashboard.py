fe612418c7021b1e18e5de710f5d5589a39d8945
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-05-17 11:03:06 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -320,4 +320,4 @@ elif selected == '📷 media':
             # Every form must have a submit button.
             submitted = st.form_submit_button("Submit")
                 if submitted:
-                st.write("slider", slider_val, "checkbox", checkbox_val)
+                    st.write("slider", slider_val, "checkbox", checkbox_val)
