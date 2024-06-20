e191e187c7e5a121a15425f5b01628f9ce21c70a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-20 13:58:52 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -144,11 +144,11 @@ from datetime import datetime
 # datetime object containing current date and time
 now = datetime.now()
  
-print("now =", now)
+st.write("now =", now)
 
 # dd/mm/YY H:M:S
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print("date and time =", dt_string)
+st.write("date and time =", dt_string)
 ###
 
 # --- CONNECT TO DETA ---
