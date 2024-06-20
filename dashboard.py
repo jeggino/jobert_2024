690c99a589bb6d39a398cac23d00df23da52b6fc
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-20 13:51:51 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -342,3 +342,16 @@ elif selected == '📷 media':
                     drive.put(f"{pict_name}", data=bytes_data)
                     insert_info(pict_name,info,project)
                     st.rerun()
+
+
+
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
